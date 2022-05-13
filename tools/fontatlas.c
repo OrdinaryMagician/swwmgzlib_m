8fc3f8da2381aa4671b72afbfdcf6a53bcd2298e
Marisa Kirisame <marisa@sayachan.org>
2022-05-13 18:30:54 +0200

    Personal rebranding.

--- tools/fontatlas.c
+++ tools/fontatlas.c
@@ -3,7 +3,7 @@
 	plaintext format that's easier to read from zscript code.
 	(https://evanw.github.io/font-texture-generator/)
 
-	Copyright (c) 2022 Marisa Kirisame, UnSX Team
+	Copyright (c) 2022 Marisa the Magician, UnSX Team
 
 	Permission is hereby granted, free of charge, to any person obtaining
 	a copy of this software and associated documentation files (the
